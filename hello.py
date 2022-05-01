f203e184e2a1d7ca57764ed9076c6957cde5ab64
Nsindiso Mnqobi Matshanga <nsindiso.matshanga@econet.co.zw>
2022-05-01 23:38:05 +0000

    added first template

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from pickle import TRUE
-from flask import Flask
+from flask import Flask, render_template
 
 # Create a Flask instance 
 app = Flask(__name__)
@@ -8,10 +8,12 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-    return ''<h1>Hello World </h1>"
+    return  render_template("index.html")
 
+@app.route("/user/<name>")
 
-
+def user(name):
+    return f"<h1>Hello {name}</h1>"
 
 
 if __name__=="main":
