9b076b264163b1def458c72862b6e9b464ddaa9a
Nsindiso Mnqobi Matshanga <nsindiso.matshanga@econet.co.zw>
2022-05-08 14:45:29 +0000

    New Commit

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
+from ast import Return
 from pickle import TRUE
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField , SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask instance 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Nsindiso'
+
+class NamerForm(FlaskForm):
+    name = StringField("Whats Your Name ? " ,validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a route decorater 
 @app.route("/")
@@ -29,6 +39,16 @@ def page_not_found(e):
 
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form = form) 
 
 
 if __name__=="main":
